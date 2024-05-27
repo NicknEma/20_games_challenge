f1263b9e4ba93613cbd9cd75c7ae2a5ba035c0f7
NicknEma <62065135+NicknEma@users.noreply.github.com>
2024-05-27 11:29:17 +0200

    Updated build.bat to include the icon

--- pong/build.bat
+++ pong/build.bat
@@ -1,3 +1,6 @@
 @echo off
 
-odin build src -debug -out:pong.exe -vet-shadowing
+rc /nologo res/resources.rc
+
+odin build src -debug -out:pong.exe -vet-shadowing -extra-linker-flags:"res/resources.res"
+del res/resources.res > NUL 2> NUL
